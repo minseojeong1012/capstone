967abf9a05a7c64c15858252a32f5020128e22ef
minseojeong <jgoldstone7@gmail.com>
2024-04-03 21:39:00 +0900

    django environ 활용

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+from environ.environ import Env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
